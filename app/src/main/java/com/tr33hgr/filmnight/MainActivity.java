1d8cc71c932800176b908ede4814b30b9de03aaa
TR33HGR <nathaniel.john.m@gmail.com>
2018-07-21 12:44:16 +0100

    Add 'Host' button to create new event

--- app/src/main/java/com/tr33hgr/filmnight/MainActivity.java
+++ app/src/main/java/com/tr33hgr/filmnight/MainActivity.java
@@ -1,13 +1,24 @@
 package com.tr33hgr.filmnight;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+    }
+
+    //Called when 'Host' button is pressed
+    public void openEventCreate(View view){
+
+        Intent openEventCreateAct = new Intent(this, EventCreateActivity.class);
+        startActivity(openEventCreateAct);
     }
 }
