fdd1252c35c8dbdfea5f98180943613ad9b13fa5
TR33HGR <nathaniel.john.m@gmail.com>
2018-07-23 11:37:29 +0100

    add event create activity and start film search bar with input text formatting

--- app/src/main/java/com/tr33hgr/filmnight/EventCreateActivity.java
+++ app/src/main/java/com/tr33hgr/filmnight/EventCreateActivity.java
@@ -2,8 +2,15 @@ package com.tr33hgr.filmnight;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class EventCreateActivity extends AppCompatActivity{
+
+    List<String> BASE_URL = Arrays.asList("http://www.omdbapi.com/?t=","&plot=full&r=xml");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +18,18 @@ public class EventCreateActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_event_create);
+    }
+
+    public void searchFilm(View view){
+
+        EditText filmText = (EditText) findViewById(R.id.searchFilmName);
+        String filmName = filmText.getText().toString();
+
+        //for search formatting
+        if(filmName.indexOf(' ') >= 0){ //if the film name contains a space character
+            filmName = filmName.replace(' ', '+'); //replace with '+'
+        };
+
+        String searchURL = BASE_URL.get(0) + filmName + BASE_URL.get(1);
     }
 }
